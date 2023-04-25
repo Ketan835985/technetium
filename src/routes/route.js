8f87de570e8f890109e61a48bd40fdf98ec6abc8
Ketan Gupta <guptaketan6375@gmail.com>
2023-04-25 21:55:23 +0530

    voting Problem

--- src/routes/route.js
+++ src/routes/route.js
@@ -159,5 +159,50 @@ router.post( "/post-query-2", function (req, res){
     res.send( {data: finalArr , status: true})
 })
 
+/*you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+also return an array consisting of only the person that can vote
+*/
+
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ 
+
+router.post('/voting', function(req,res){
+    let VotingAge = req.query.votingAge
+    // console.log(VotingAge)
+    let filterAge = persons.filter(x=>{
+        if(x.age>=VotingAge){
+            return x["votingStatus"] = true
+
+        }
+    })
+    res.send({data: filterAge, status :true})
+})
 
 module.exports = router;
